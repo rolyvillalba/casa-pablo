a3bd4d2d6dc12ea2048c1aa274bb7552d57d9824
rolyvillalba <rolyvillalba@gmail.com>
2022-08-02 03:08:38 -0300

    first commit

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama-pablo",
+      "name": "PANORAMA PABLO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.3267073990222737,
+        "pitch": 0.03675315902163412,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "LIVING/COMEDOR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
